aae857af79e69b35f49bfa0c4df3ddb73330993b
Eric Crudup <briefcasemanx@gmail.com>
2022-01-14 11:07:48 -0800

    WIP: still working on appearing/disappearing, just fixed a typo that caused confusion for over an hour

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
-
   const name = $("input#name").val();
   const age = $("input#age").val();
   const personality = $("input#personality").val();
   const organized = $("input#organized").val();
   const favSubject = $("input#favSubject").val();
  
-  $("form#calculator").submit(function(event) {
+  $("form#userInfo").submit(function(event) {
+    event.preventDefault;
+    if ( 1 === 1  )
+    { 
+      console.log(document.getElementById("question1").style.display);
+      document.getElementById("question1").style.display = "none";
+      console.log("im here");
+    }
   });
 });
