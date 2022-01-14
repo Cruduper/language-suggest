101cc72f70897265bcc95e7bfda6b458337102e1
Eric Crudup <briefcasemanx@gmail.com>
2022-01-14 11:35:04 -0800

    added branching to get questions to appear/disappear, but can't find a way to store user input into const variables with current code structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,48 @@
 $(document).ready(function() {
-  const name = $("input#name").val();
-  const age = $("input#age").val();
-  const personality = $("input#personality").val();
-  const organized = $("input#organized").val();
-  const favSubject = $("input#favSubject").val();
+  let name;
+  let age;
+  let personality;
+  let organized;
+  let favSubject;
  
   $("form#userInfo").submit(function(event) {
+
     event.preventDefault;
-    if ( 1 === 1  )
+
+    if ( document.getElementById("question1").style.display != "none" )
     { 
-      console.log(document.getElementById("question1").style.display);
-      document.getElementById("question1").style.display = "none";
       console.log("im here");
+      document.getElementById("question1").style.display = "none";
+      name = $("input#name").val()
+      document.getElementById("question2").style.display = "block";
+    } 
+    else if ( document.getElementById("question2").style.display != "none" ) {
+
+        console.log("im here 2");
+        document.getElementById("question2").style.display = "none";
+        age = $("input#age").val();
+        document.getElementById("question3").style.display = "block";
+    } 
+    else if ( document.getElementById("question3").style.display != "none" ) {
+
+        console.log("im here 3");
+        document.getElementById("question3").style.display = "none";
+        personality = $("input#personality").val();
+        document.getElementById("question4").style.display = "block";
+    } 
+    else if ( document.getElementById("question4").style.display != "none" ) {
+
+        console.log("im here 4");
+        document.getElementById("question4").style.display = "none";
+        organized = $("input#organized").val();
+        document.getElementById("question5").style.display = "block";
+    } 
+    else {
+
+        console.log("im here 5");
+        document.getElementById("question5").style.display = "none";
+        favSubject = $("input#favSubject").val();
+        document.getElementById("result").style.display = "block";
     }
   });
 });
