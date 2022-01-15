538426ff9416fac014fa34be8fa14bbb47beb5f6
Eric Crudup <briefcasemanx@gmail.com>
2022-01-14 17:27:18 -0800

    changed page title styling, added placeholder= text in input instead of value= text

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 function findLanguage( _name, _age, _favFood, _organized, _favSubject)  {
 
+  // easter egg, try "Jabroni" as the name, with a capital J
   if (_name != "Jabroni") {
     if( _age < 60 ) {
       if(_favFood === "nachos")  {
@@ -63,8 +64,12 @@ $(document).ready(function() {
       name = $("input#name").val();
       
       if (name === "Jabroni") {
-        $("#alt-text").show();
-        $("#alt-text").fadeOut(3000);
+        $("#alt-text1").show();
+        $("#alt-text1").fadeOut(3000);
+      }
+      else if (name === "John Doe") {
+        $("#alt-text2").show();
+        $("#alt-text2").fadeOut(3000);
       }
 
       $("#question2").show();
